dfa91107302d16566feb8c91419dce5469c1d826
Shivam Sharma <shivasharmarag@gmail.com>
2025-01-13 21:15:49 +0530

    Added signup and signin routes

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://shivasharmarag:qoW7JYsvb2cWrZ52@cluster0.sg6th.mongodb.net/Brainly");
+mongoose.connect("mongodb+srv://shivasharmarag:qoW7JYsvb2cWrZ52@cluster0.sg6th.mongodb.net/Paytm");
 
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
