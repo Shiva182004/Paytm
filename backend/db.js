199dc15c79fef8a14a3333e36a094c09f613e209
Shivam Sharma <shivasharmarag@gmail.com>
2025-01-13 08:47:10 +0530

    Initial commit

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+// backend/db.js
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://shivasharmarag:qoW7JYsvb2cWrZ52@cluster0.sg6th.mongodb.net/Brainly");
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+// Create a model from the schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User
+};
